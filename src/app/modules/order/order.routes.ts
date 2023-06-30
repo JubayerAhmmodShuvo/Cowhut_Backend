f445ed275cdbbe92d33177cef93544e331ab7bba
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-06-30 20:21:09 +0600

    order route is not working

--- src/app/modules/order/order.routes.ts
+++ src/app/modules/order/order.routes.ts
@@ -3,7 +3,7 @@ import { OrderController } from './order.controller';
 
 const router = express.Router();
 
-router.get('/', OrderController.getOrdersController);
 router.post('/', OrderController.createOrderController);
+router.get('/', OrderController.getOrdersController);
 
 export const OrderRoutes = router;
