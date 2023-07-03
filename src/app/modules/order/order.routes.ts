2f581918496affedd56a9d0b75e77368fd51e350
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-07-03 16:40:54 +0600

    readme updating

--- src/app/modules/order/order.routes.ts
+++ src/app/modules/order/order.routes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { OrderController } from './order.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '../../../enum/user';
 
 const router = express.Router();
 
-router.post('/', OrderController.createOrderController);
-router.get('/:id', OrderController.getOrderByIdController);
-router.get('/', OrderController.getOrdersController);
+router.post('/',auth(UserRole.Buyer), OrderController.createOrderController);
+router.get(
+  '/:id',
+  auth(UserRole.Buyer),
+  OrderController.getOrderByIdController
+);
+router.get('/', auth(UserRole.Buyer), OrderController.getOrdersController);
 
 export const OrderRoutes = router;
