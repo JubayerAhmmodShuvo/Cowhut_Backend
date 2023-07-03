e5cedd1c7de6a0b901bcee4d7b7a6af6d6679e6e
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-07-03 18:08:36 +0600

    user and admin routes added

--- src/app/modules/user/users.routes.ts
+++ src/app/modules/user/users.routes.ts
@@ -6,22 +6,22 @@ import { UserRole } from '../../../enum/user';
 
 const router = express.Router();
 
-router.get('/', auth(UserRole.Admin), UserController.getAllUsers);
 
 router.get(
   '/my-profile',
   auth(UserRole.Buyer, UserRole.Seller),
   UserController.getMyProfile
-);
-router.patch(
-  '/my-profile',
-  auth(UserRole.Buyer, UserRole.Seller),
-  UserController.updateMyProfile
-);
-
-router.get('/:id', auth(UserRole.Admin), UserController.getUserById);
-
-router.patch('/:id', auth(UserRole.Admin), UserController.updateUserById);
-router.delete('/:id', auth(UserRole.Admin), UserController.deleteUserById);
+  );
+  router.patch(
+    '/my-profile',
+    auth(UserRole.Buyer, UserRole.Seller),
+    UserController.updateMyProfile
+    );
+    
+    router.get('/:id', auth(UserRole.Admin), UserController.getUserById);
+    
+    router.patch('/:id', auth(UserRole.Admin), UserController.updateUserById);
+    router.delete('/:id', auth(UserRole.Admin), UserController.deleteUserById);
+    router.get('/', auth(UserRole.Admin), UserController.getAllUsers);
 
 export const UserRoutes = router;
